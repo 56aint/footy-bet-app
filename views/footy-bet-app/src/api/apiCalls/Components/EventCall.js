41eb6a5054f312d26bb8a816632ba78373b5bad7
aar.56aint <aaron.hays_7@yahoo.com>
2021-09-05 15:55:38 +0100

    single event component unrefactored

--- views/footy-bet-app/src/api/apiCalls/Components/EventCall.js
+++ views/footy-bet-app/src/api/apiCalls/Components/EventCall.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import socketConnection from '../../connection';
 import NavBar from '../../../Component/NavBar.';
 import '../../../Styles/FootyEvent.css';
@@ -6,14 +6,17 @@ import '../../../Styles/FootyEvent.css';
 const ws = socketConnection();
 
 export default function GetEvent() {
-  const [matchTime, setMatchTime] = useState('');
-  const [teamNames, setTeamNames] = useState('');
+  // const [stored, setStored] = useState([]);
+  // const [matchTime, setMatchTime] = useState('');
+  // const [teamNames, setTeamNames] = useState('');
+  // const [market, setMarket] = useState('');
   const [isLoading, setLoading] = useState(true);
-  /* const [socketData, setSocketData] = useState([]); */
+  const [socketData, setSocketData] = useState([]); // array of objects.
 
   useEffect(() => {
-    ws.onopen = () => ws.send(
-      /* eslint-disable */
+    ws.onopen = () => {
+      ws.send(
+        /* eslint-disable */
         JSON.stringify({
           type: 'subscribe',
           keys: ['e.21249934'],
@@ -22,48 +25,71 @@ export default function GetEvent() {
           id: 21249934,
         }),
       );
-
+      ws.send(
+        /* eslint-disable */
+        JSON.stringify({
+          type: 'subscribe',
+          keys: ['m.93648663'],
+          type: 'getMarket',
+          id: 93648663,
+        }),
+      );
+    };
     function handleMessage(event) {
       console.log('Handling message');
       setLoading(true);
       const parsedData = JSON.parse(event.data);
-      console.log(parsedData);
-      setMatchTime((parsedData.data.startTime));
-      setTeamNames(parsedData.data.name);
-      /* setSocketData((currentSocketData) => [...currentSocketData, parsedData]); */
+      // console.log(parsedData);
+      setSocketData((currentSocketData) => [...currentSocketData, parsedData]);
       setLoading(false);
     }
-
 
     ws.addEventListener('message', handleMessage);
 
     return () => ws.removeEventListener('message', handleMessage);
-
   }, []);
-  // console.log(matchTime);
+
+
+  const eventTime = socketData.map((x, index, arr) => {
+    if (x.type === 'EVENT_DATA') {
+      return x.data.startTime
+    } 
+  })
+  const eventTeams = socketData.map((x, index, arr) => {
+    if (x.type === 'EVENT_DATA') {
+      return x.data.name
+    } 
+  })
+  const eventBet = socketData.map((x, index, arr) => {
+    if (x.type === 'MARKET_DATA') {
+      return x.data.name
+    } 
+  })
   return (
     <>
-    <NavBar />
+      <NavBar />
       <div className="footyevent" data-testid="footy-event-id">
         <div className="container">
           <h1>Football</h1>
           <div className="title-box">
-          {isLoading && <div className="loading">Not Connected... Please Refresh</div>}
-          </div>
-          <div className="title-box">
-            <div className="startTime" data-testid="event-time-id">
-              Date & Time: {matchTime}
+            <div key="uniqueId1">
+            {isLoading && <div className="loading">Not Connected... Please Refresh</div>}
             </div>
           </div>
           <div className="title-box">
-            <div className="teams" data-testid="playing-teams-id">
-              {teamNames}
+            <div key="uniqueId2" className="teams" data-testid="playing-teams-id">
+              <p>Date: {eventTime}</p>
             </div>
           </div>
           <div className="title-box">
-            {/* <div className="socket-data" data-testid="socket-data-id">
-              <div>{socketData.map((x) => x.toString())}</div>
-            </div> */}
+            <div key="uniqueId2" className="teams" data-testid="playing-teams-id">
+              <p>Teams: {eventTeams}</p>
+            </div>
+          </div>
+          <div className="title-box">
+            <div key="uniqueId2" className="teams" data-testid="playing-teams-id">
+              <p>BET: {eventBet}</p>
+            </div>
           </div>
         </div>
       </div>
--- views/footy-bet-app/src/api/connection.js
+++ views/footy-bet-app/src/api/connection.js
@@ -15,11 +15,20 @@ export default function socketConnection() {
   // ws.addEventListener("message", (e) => console.log(e.data)); // logs all data to console
 
   ws.onmessage = function (message) {
-    console.log('Received:', message.data);
+    console.log('This is the received data:', message.data);
   }; // logs received data to console
 
-  ws.onclose = function (event) {
-    console.log('Connection closed!: ', event);
-  }; // logs connection close to console
+  ws.onclose = function(event) {
+    if (event.wasClean) {
+      alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    } else {
+      // e.g. server process killed or network down
+      alert('[close] Connection died');
+    }
+  }; // logs connection closed cleanly to the console
+
+  ws.onerror = function(error) {
+    alert(`[error] ${error.message}`);
+  };
   return ws;
 }
